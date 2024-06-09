92c67c2915270e4a51d3bfe321bb79835b52f4b0
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-09 11:47:21 -0400

    Create post model

---
+++ src/models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+   author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+   text: { type: String, required: true },
+   timestamp: { type: Date, default: Date.now },
+   publish_date: { type: Date },
+   is_published: { type: Boolean, required: true },
+});
+
+PostSchema.virtual("timestamp_formatted").get(function () {
+   return this.timestamp.toLocaleString();
+});
+
+PostSchema.virtual("publish_date_formatted").get(function () {
+   return this.publish_date.toLocaleTimeString();
+});
+
+module.exports = mongoose.model("Post", PostSchema);
