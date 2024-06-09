5f0a716026687adfdd231fc7cadfad17a0d09e00
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-09 12:02:38 -0400

    Add a pre hook on model to hash password then store it

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
    name: { type: String, required: true },
    email: { type: String, required: true, unique: true, lowercase: true },
    password: { type: String, required: true },
    is_author: { type: Boolean, required: true },
+});
+
+UserSchema.pre("save", async function (next) {
+   const hash = await bcrypt.hash(this.password, 10);
+   this.password = hash;
+
+   next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
