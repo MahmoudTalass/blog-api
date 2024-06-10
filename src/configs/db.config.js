39d5aaf374690ea8e737cb2eb3705f0b6e6f5834
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-10 18:29:00 -0400

    Adjust error send to user by database connection error

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
-connectDB().catch((err) => app.sendStatus(500).json({ message: "server error" }));
+connectDB().catch((err) =>
+   app
+      .sendStatus(500)
+      .json({ status: 500, message: "Something went wrong. Please try again later" })
+);
 async function connectDB() {
    await mongoose.connect(process.env.MONGODB_URI);
 }
