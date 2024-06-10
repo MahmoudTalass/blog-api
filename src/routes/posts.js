df4b34aa46cc66ffda398ba065ac0b325935e726
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-10 18:24:04 -0400

    Apply controller functions to routes

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,34 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const postsController = require("../controllers/posts_controller");
 
 /**
  * retrieve all posts
  */
-router.get("/");
+router.get("/", postsController.getAllPosts);
 
 /**
  * retrieve a particular post
  */
-router.get("/:postId");
+router.get("/:postId", postsController.getPost);
 
 /**
  * retrieve all comments for a particular post
  */
-router.get("/:postId/comments");
+router.get("/:postId/comments", postsController.getPostComments);
 
 /**
  * create a new post
  */
-router.post("/");
+router.post("/", postsController.createPost);
 
 /**
  * update a particular post
  */
-router.put("/:postId");
+router.put("/:postId", postsController.updatePost);
 
 /**
  * delete a particular post
  */
-router.delete("/:postId");
+router.delete("/:postId", postsController.deletePost);
 
 module.exports = router;
