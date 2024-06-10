2ac8539e2806ce7d3430ad98cf76e2664db3dfcd
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-10 14:18:36 -0400

    Create endpoint to get a particular post

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,27 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 /**
- * Endpoint to retrieve all posts
+ * retrieve all posts
  */
 router.get("/");
 
 /**
- * Endpoint to retrieve all comments for a particular post
+ * retrieve a particular post
+ */
+router.get("/:postId");
+
+/**
+ * retrieve all comments for a particular post
  */
 router.get("/:postId/comments");
 
 /**
- * Endpoint to create a new post
+ * create a new post
  */
 router.post("/");
 
 /**
- * Endpoint to update a particular post
+ * update a particular post
  */
 router.put("/:postId");
 
 /**
- * Endpoint to delete a particular post
+ * delete a particular post
  */
 router.delete("/:postId");
 
