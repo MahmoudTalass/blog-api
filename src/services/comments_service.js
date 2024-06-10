64c2cf4b85bed27affbf1dd2e15782901d4dd2a9
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-10 19:08:15 -0400

    Create comments service to handle DB CRUD operations

---
+++ src/services/comments_service.js
@@ -0,0 +1,60 @@
+const Comment = require("../models/comment");
+const AppError = require("../utils/app_error");
+
+class CommentsService {
+   async getAllComments() {
+      await Comment.find().populate("author").exec();
+   }
+
+   async getComment(commentId) {
+      const comment = await Comment.findById(commentId);
+
+      if (comment === null) {
+         throw new AppError("Comment not found", 404);
+      }
+
+      return comment;
+   }
+
+   async createComment(authorId, text, postId) {
+      const comment = new Comment({
+         author: authorId,
+         text,
+         post_id: postId,
+      });
+
+      await comment.save();
+   }
+
+   async updateComment(authorId, text, postId, commentId) {
+      const comment = await Comment.findById(commentId).exec();
+
+      if (comment === null) {
+         throw new AppError("Comment not found", 404);
+      }
+
+      const updatedComment = {
+         author: authorId,
+         text,
+         post_id: postId,
+      };
+
+      await Comment.findByIdAndUpdate(
+         commentId,
+         { $set: updatedComment },
+         { runValidators: true }
+      ).exec();
+   }
+
+   async deleteComment(commentId) {
+      const comment = await Comment.findById(commentId).exec();
+
+      if (comment === null) {
+         throw new AppError("Comment not found", 404);
+      }
+
+      await Post.findByIdAndDelete(commentId).exec();
+   }
+}
+
+module.exports = new CommentsService();
