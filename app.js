0e98601f51bed953aa1059734e09cbf748e288be
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-12 18:46:54 -0400

    Require passport config and initialize passport

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ const createError = require("http-errors");
 const express = require("express");
 const errorHandler = require("./src/middlewares/error_handler");
 const cors = require("cors");
+const passport = require("passport");
 
 require("dotenv").config();
-require("./src/configs/db.config");
 
 const indexRouter = require("./src/routes/index");
 
 const app = express();
 
+// DB configuration
+require("./src/configs/db.config");
+
+// Passport configuration
+require("./src/configs/passport.config");
+app.use(passport.initialize());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+// API routes
 app.use("/api", indexRouter);
 
 // error handler
