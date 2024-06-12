d92297fb9f448f60600aa483e13f135d198c95b5
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-12 09:47:08 -0400

    set status instead of immediately sending it

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 connectDB().catch((err) =>
-   app
-      .sendStatus(500)
-      .json({ status: 500, message: "Something went wrong. Please try again later" })
+   app.status(500).json({ status: 500, message: "Something went wrong. Please try again later" })
 );
 async function connectDB() {
    await mongoose.connect(process.env.MONGODB_URI);
