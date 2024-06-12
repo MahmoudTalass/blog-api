49b9543821a7f598c6f462a86f077cffbe24497c
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-12 18:45:33 -0400

    Create user routes for accessing user related data

---
+++ src/routes/user.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user_controller");
+const passport = require("passport");
+
+/**
+ * retrieve current user info
+ */
+router.get(
+   "/",
+   passport.authenticate("jwt", { session: false }),
+
+   userController.getCurrentUserInfo
+);
+
+/**
+ * retrieve all posts from current user
+ */
+router.get("/posts", passport.authenticate("jwt", { session: false }), userController.getUserPosts);
+
+/**
+ * retrieve all comments by from current user
+ */
+router.get(
+   "/comments",
+   passport.authenticate("jwt", { session: false }),
+   userController.getUserPosts
+);
+
+/**
+ * retrieve all comments by from a particular user
+ */
+router.get("/:userId/comments", userController.getUserPosts);
+
+/**
+ * retrieve all posts by from a particular user
+ */
+router.get("/:userId/posts", userController.getUserPosts);
+
+module.exports = router;
