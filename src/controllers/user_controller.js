4652ba301fb6d8c60cbc652efed11c12797d6c48
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-12 18:46:03 -0400

    Create user controller to handle user data endpoints

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,36 @@
+const asyncHandler = require("express-async-handler");
+const UserService = require("../services/user_service");
+
+const getCurrentUserInfo = (req, res, next) => {
+   const user = req.user;
+   res.json({
+      name: user.name,
+      email: user.email,
+      isAuthor: user.isAuthor,
+      id: user.id,
+   });
+};
+
+const getCurrentUserPosts = asyncHandler(async (req, res, next) => {
+   return await UserService.getUserPosts(req.user.id);
+});
+
+const getCurrentUserComments = asyncHandler(async (req, res, next) => {
+   return await UserService.getUserComments(req.user.id);
+});
+
+const getUserPosts = asyncHandler(async (req, res, next) => {
+   return await UserService.getUserPosts(req.params.userId);
+});
+
+const getUserComments = asyncHandler(async (req, res, next) => {
+   return await UserService.getUserComments(req.params.userId);
+});
+
+module.exports = {
+   getCurrentUserInfo,
+   getCurrentUserPosts,
+   getCurrentUserComments,
+   getUserPosts,
+   getUserComments,
+};
