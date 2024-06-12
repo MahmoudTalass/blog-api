4ed85aae1b989e8cec3255c2acfb8c347cec7fc6
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-12 18:10:40 -0400

    Refactor to allow users with invalid tokens into signin/login routes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -14,11 +14,16 @@ const checkNotAuthenticated = asyncHandler((req, res, next) => {
    const authHeader = req.headers["authorization"];
    const token = authHeader && authHeader.split(" ")[1];
 
-   if (token === null) {
-      return next();
+   if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+         if (err) {
+            next();
+         }
+         throw new AppError("You are already logged in", 403);
+      });
    }
 
-   throw new AppError("You are already logged in", 403);
+   return next();
 });
 
 module.exports = {
