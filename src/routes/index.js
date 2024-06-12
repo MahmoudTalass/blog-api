57682c56c52e265c67150373e3d11d24492eae2d
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-12 18:46:25 -0400

    Import auth and user routers and attach them to api router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,12 @@ const router = express.Router();
 
 const postsRouter = require("./posts");
 const commentsRouter = require("./comments");
+const authRouter = require("./auth");
+const userRouter = require("./user");
 
+router.use("/auth", authRouter);
 router.use("/posts", postsRouter);
 router.use("/comments", commentsRouter);
+router.use("/user", userRouter);
 
 module.exports = router;
