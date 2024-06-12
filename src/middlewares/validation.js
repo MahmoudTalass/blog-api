885099accb9c341bd45efeb510aca0d640acb713
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-11 20:10:14 -0400

    Create validation for login and signup inputs

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,4 +1,6 @@
 const { body, validationResult } = require("express-validator");
+const User = require("../models/user");
+const { AppError } = require("../utils/app_error");
 const ValidationError = require("../utils/app_error").ValidationError;
 
 const validatePost = [
@@ -17,7 +19,73 @@ const validatePost = [
          const error = errors.array().map((err) => {
             return { message: err.msg };
          });
-         return next(new ValidationError(errors));
+         return next(new ValidationError(error));
+      }
+
+      next();
+   },
+];
+
+const validateSignup = [
+   body("name", "Must provide a name").trim().notEmpty().escape(),
+   body("email", "Must provide an email")
+      .trim()
+      .notEmpty()
+      .isEmail()
+      .withMessage("Must provide a valid email")
+      .escape()
+      .custom(async (value) => {
+         const user = await User.findOne({ email: value }).exec();
+         if (user) {
+            throw new AppError("User with this email already exists", 409);
+         }
+         return true;
+      }),
+   body("password", "Must provide a password")
+      .trim()
+      .isLength({ min: 8 })
+      .withMessage("Password must contain at least 8 characters")
+      .escape(),
+   body("isAuthor")
+      .customSanitizer((value) => {
+         if (value === "yes") return true;
+         return false;
+      })
+      .escape(),
+   (res, req, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+         const error = errors.array().map((err) => {
+            return { message: err.msg };
+         });
+         return next(new ValidationError(error));
+      }
+
+      next();
+   },
+];
+
+const validateLogin = [
+   body("email", "Must provide an email")
+      .trim()
+      .notEmpty()
+      .isEmail()
+      .withMessage("Must provide a valid email")
+      .escape(),
+   body("password", "Must provide a password")
+      .trim()
+      .isLength({ min: 8 })
+      .withMessage("Password must contain at least 8 characters")
+      .escape(),
+   (res, req, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+         const error = errors.array().map((err) => {
+            return { message: err.msg };
+         });
+         return next(new ValidationError(error));
       }
 
       next();
@@ -26,4 +94,6 @@ const validatePost = [
 
 module.exports = {
    validatePost,
+   validateSignup,
+   validateLogin,
 };
