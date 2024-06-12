7eac3d8635f4d8a6b911d368b7a7dc7bca043134
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-12 09:09:36 -0400

    Create middleware for authorization/authentication

---
+++ src/middlewares/auth.js
@@ -0,0 +1,32 @@
+const asyncHandler = require("express-async-handler");
+const { AppError } = require("../utils/app_error");
+const jwt = require("jsonwebtoken");
+
+const isAuthor = asyncHandler((req, res, next) => {
+   if (req.user.isAuthor) {
+      next();
+   } else {
+      throw new AppError("Unauthorized access", 403);
+   }
+});
+
+const checkNotAuthenticated = asyncHandler((req, res, next) => {
+   const authHeader = req.headers["authorization"];
+   const token = authHeader && authHeader.split(" ")[1];
+
+   if (token === null) {
+      return next();
+   }
+   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+         return next();
+      }
+
+      throw new AppError("You are already logged in", 403);
+   });
+});
+
+module.exports = {
+   isAuthor,
+   checkNotAuthenticated,
+};
