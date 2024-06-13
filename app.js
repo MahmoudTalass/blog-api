486e5a3134200c30c173e8c55c7f65f89f2b192c
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-13 09:58:33 -0400

    Install useful production packages and mount them on express app

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const errorHandler = require("./src/middlewares/error_handler");
 const cors = require("cors");
 const passport = require("passport");
 
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+   windowMs: 1 * 60 * 1000, // 1 minute
+   max: 20,
+});
+
 require("dotenv").config();
 
 const indexRouter = require("./src/routes/index");
@@ -16,6 +24,10 @@ require("./src/configs/db.config");
 // Passport configuration
 require("./src/configs/passport.config");
 app.use(passport.initialize());
+
+app.use(compression());
+app.use(helmet());
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
