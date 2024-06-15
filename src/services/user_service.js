5f5fab75c587d2a8837f6dd11a39806d2edcf46e
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-15 16:30:37 -0400

    Check validity of user id provided in params

--- src/services/user_service.js
+++ src/services/user_service.js
@@ -1,5 +1,7 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
+const { AppError } = require("../utils/app_error");
+const isValid = require("mongoose").Types.ObjectId.isValid;
 
 class UserService {
    async getCurrentUserPosts(userId) {
@@ -9,6 +11,10 @@ class UserService {
          .exec();
    }
    async getUserPosts(userId) {
+      if (!isValid(userId)) {
+         throw new AppError("User not found", 404);
+      }
+
       return await Post.find({ author: userId, isPublished: true })
          .populate("author", "name email isAuthor")
          .sort({ createdAt: -1, _id: 1 })
@@ -16,6 +22,10 @@ class UserService {
    }
 
    async getUserComments(userId) {
+      if (!isValid(userId)) {
+         throw new AppError("User not found", 404);
+      }
+
       return await Comment.find({ author: userId })
          .populate("author", "name email isAuthor")
          .sort({ createdAt: -1, _id: 1 })
